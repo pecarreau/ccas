24a58f4a995ee2fb6d5285b67365befd363b5f5b
Archibald Roulier <archibald.roulier@student-cs.fr>
2022-04-20 18:55:26 +0200

    galère de branche main/master

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,12 +9,11 @@ from django.http import Http404
 import requests
 
 def index(request):
-    url = "https://mes-aides.1jeune1solution.beta.gouv.fr/api/simulation/via/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNWVhZDAwMTQyYWVkNDc4ZmMzNTllOSIsInNjb3BlIjoiY2Nhc19zYWludF9sb3Vpc19wcmVwcm9kIiwiZXhwIjoxNjUwMzc1NDUyLCJpYXQiOjE2NTAzNzE4NTJ9.EFYCLQHru3PVOK-1Kpkc1X3sNu0UEvIDrJ41PbvwBnY"
+    url = "https://mes-aides.1jeune1solution.beta.gouv.fr/api/simulation/via/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjAxOWJlN2YyM2JhNzgwMGQwNDAwNCIsInNjb3BlIjoiY2Nhc19zYWludF9sb3Vpc19wcmVwcm9kIiwiZXhwIjoxNjUwNDY4ODI1LCJpYXQiOjE2NTA0NjUyMjV9._naULQ2aeQD5QfdXXiaNPOAPIKU4qQNIG4SJKFWTrk4"
     response = requests.get(url, params= "id=openfisca" )
     content=response.json()
     aides_eligible = []
-    for cle in content : 
-        
+    for cle in content :    
         content_key= content[cle]
         for item in content_key :
             key_aides = []
@@ -27,14 +26,37 @@ def index(request):
                     aides_eligible.append(key)
     return render(request,'polls/index.html',{'content' : content, 'content_individus_demandeur' : content_key, 'aides_eligible' : aides_eligible })
 
+def index1(request):
+    url = "https://mes-aides.1jeune1solution.beta.gouv.fr/api/simulation/via/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNjAxOWJlN2YyM2JhNzgwMGQwNDAwNCIsInNjb3BlIjoiY2Nhc19zYWludF9sb3Vpc19wcmVwcm9kIiwiZXhwIjoxNjUwNDY4ODI1LCJpYXQiOjE2NTA0NjUyMjV9._naULQ2aeQD5QfdXXiaNPOAPIKU4qQNIG4SJKFWTrk4"
+    response = requests.get(url, params= "id=openfisca" )
+    content=response.json()
+    url2 = 'https://mes-aides.org/api/benefits'
+    content2 = requests.get(url2).json() #content est une liste de dictionnaire (chacun correspond à une aide)
+    liste_aide=[]
+    liste_eligibilite = []
+    for aide in content2 :
+        if aide['source']=='openfisca':
+            liste_aide.append(aide['id'])
+
+    for cle in content:
+        for key in content[cle]:
+            for item in content[cle][key]:
+                x=content[cle][key][item]
+                
+                liste_cle  = []
+                for val in x : 
+                    liste_cle.append(val)
+                if '2022-04' in liste_cle : 
+                        if (x['2022-04'] == True or x['2022-04'] == 1) and item in liste_aide:
+                            liste_eligibilite.append(item)
+    return render(request,'polls/index1.html',{ 'liste_eligibilite' : liste_eligibilite })
 '''
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 #context est un argument qui référence les variables pouvant être appelée dans le fichier html
-'''
-'''
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
@@ -42,12 +64,10 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
-'''
-
-""""
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
-"""
+'''
+
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
