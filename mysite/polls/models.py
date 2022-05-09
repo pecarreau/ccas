0c24a76f640695fb4e9c68fcd40f47de48f1f8a7
Garampon Theo <theo.garampon@student-cs.fr>
2022-05-09 13:19:18 +0200

    base de données

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -14,3 +14,24 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+    
+class Aides(models.Model):
+    name = models.CharField(max_length = 200, unique = False)
+    price = models.IntegerField(max_length = 15, unique = False, blank = True)
+    
+class Recipients(models.Model):
+    id = models.CharField(max_length = 200, primary_key = True)
+    name = models.CharField(max_length = 200, unique = False)
+    surname = models.CharField(maxlength = 200, unique = False)
+    birthday = models.DateField()
+    email = models.EmailField(max_length = 254)
+    allowed_social_support = models.ManyToManyField(Aides)
+    possessed_social_support = models.ManyToManyField(Aides)
+    needed_social_support = models.ManyToManyField(Aides)
+    def __str__(self):
+        return self.id
+    
+class Simulations(models.Model):
+    date = models.DateField()
+    recipient = models.ForeignKey(Recipients, on_delete = models.CASCADE)
+    list_of_support = models.ExpressionList()
